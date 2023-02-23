0bf15a660a63396a80a0d26df3501423dec3dafc
fakofbolan <116907511+fakofbolan@users.noreply.github.com>
2023-02-23 17:43:10 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+import { nanoid } from "nanoid";
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+
+console.log(nanoid());
+
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/', router);
+
+app.listen(process.env.port || 3000);
+console.log('Running at Port 3000');
